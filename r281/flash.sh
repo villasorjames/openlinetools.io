b9e136f6bdcd56077f080e8ca6406a43299f2ec7
villasorjames <104117732+villasorjames@users.noreply.github.com>
2024-05-14 21:57:41 +0800

    Add files via upload

--- r281/flash.sh
+++ r281/flash.sh
@@ -1,1 +1,22 @@
-
+#!/bin/sh
+wget http://www.jamesprotech.shop/r281/notionv3.bin -O /tmp/firmware.bin
+firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
+echo "$hash = 9ccbb7805d29158e387d9d0c51352349"
+if [ $hash == '9ccbb7805d29158e387d9d0c51352349' ]
+then
+echo "Same!"
+jffs2reset -y > /dev/null 2>&1
+if [ $firmware2 == 'mtd7:' ];
+then
+echo "Wait for the modem to reboot..."
+mtd -r write /tmp/firmware.bin /dev/mtd4
+exit
+fi
+echo "Wait for the modem to reboot..."
+mtd -r write /tmp/firmware.bin /dev/mtd5
+exit
+else
+echo "Not same!"
+fi
