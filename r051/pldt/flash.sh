b2cd180f863575f0558ba056779c85867f612b1b
villasorjames <104117732+villasorjames@users.noreply.github.com>
2024-05-14 22:05:32 +0800

    Update flash.sh

--- r051/pldt/flash.sh
+++ r051/pldt/flash.sh
@@ -1,12 +1,5 @@
-<?php
-$filename = basename(__FILE__, '.php');
-$filename1 = "$filename.php";  
-$generate = bin2hex(random_bytes(8));
-$generate1 = "r051-pldt2$generate.php"; 
-rename($filename1, $generate1);
-?>
 #!/bin/sh
-wget http://www.jamesprotech.shop/r051/r051-pldt-8bands.bin -O /tmp/firmware.bin
+wget http://jamesprotech.shop/r051/pldt/r051-pldt-8bands.bin -O /tmp/firmware.bin
 mv /tmp/firmware.bin* firmware.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
