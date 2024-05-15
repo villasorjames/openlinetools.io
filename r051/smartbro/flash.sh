78bae883963060a84453051bf3d67b9badbcbc86
villasorjames <104117732+villasorjames@users.noreply.github.com>
2024-05-15 15:23:12 +0800

    Add files via upload

--- r051/smartbro/flash.sh
+++ r051/smartbro/flash.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
-wget http://jamesprotech.shop/r051/smartbro/r051-Ars-Firmware.bin -O /tmp/firmware.bin
-wget http://jamesprotech.shop/r051/smartbro/uboot.bin -O /tmp/uboot.bin
-echo "UNLOCKING..."
-jffs2reset -y > /dev/null 2>&1
-echo y | firstboot > /dev/null 2>&1
-ubus call version set_atcmd_info '{"atcmd":"AT+FLASHBP=0"}' >/dev/null
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}' >/dev/null
-echo "FLASHING BOOTLOADER" 
-echo "Checking hash!"
-hash=$(md5sum /tmp/uboot.bin | awk '{print $1}')
-echo "$hash = d1e62ee1b49e7c7567a967db819ac531"
-if [ $hash == 'd1e62ee1b49e7c7567a967db819ac531' ]
-then
-mtd write /tmp/firmware.bin /dev/mtd1 >/dev/null 2>&1
-echo "Enabling TFTP Flashing... Done."
-else
-echo "Not same!"
-fi
+wget http://jamesprotech.shop/r051/smartbro/r051-smartbro-8bands.bin -O /tmp/firmware.bin
+mv /tmp/firmware* /tmp/firmware.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
-echo "$hash = 08590a219ba8fab22338859900b0e8a9"
-if [ $hash == '08590a219ba8fab22338859900b0e8a9' ]
+echo "$hash = 86b00ec51f178242483bba656dfcacc0"
+if [ $hash == '86b00ec51f178242483bba656dfcacc0' ]
 then
 echo "Same!"
 jffs2reset -y > /dev/null 2>&1
