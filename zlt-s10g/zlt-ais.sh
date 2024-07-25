26a81da5ef7c7d3c9cee988a8c3adc18a169dab7
villasorjames <104117732+villasorjames@users.noreply.github.com>
2024-07-26 00:13:13 +0800

    Add files via upload

---
+++ zlt-s10g/zlt-ais.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+curl http://link.com/zlt/ais.tgz -o /tmp/firmware.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
+echo "$hash = d2fc57234bf2ce6609682d9790a6d642"
+if [ $hash == 'd2fc57234bf2ce6609682d9790a6d642' ]
+then
+echo "Same!"
+mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
+tar -zxvf /tmp/firmware.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
