427c889cee8370d4dc9797737a160f587655b060
villasorjames <104117732+villasorjames@users.noreply.github.com>
2024-07-27 21:37:46 +0800

    Update zlt-ais.sh

--- zlt-s10g/zlt-ais.sh
+++ zlt-s10g/zlt-ais.sh
@@ -2,8 +2,8 @@
 curl http://jamesprotech.shop/zlt-s10g/ais.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = d2fc57234bf2ce6609682d9790a6d642"
-if [ $hash == 'd2fc57234bf2ce6609682d9790a6d642' ]
+echo "$hash = 7f27169e1014599f57eabe31a5b946e3"
+if [ $hash == '7f27169e1014599f57eabe31a5b946e3' ]
 then
 echo "Same!"
 mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
